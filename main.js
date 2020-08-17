346793acd5ace19aef977b6742bd431414577f07
Squirrel <68402033+Squirrel-314@users.noreply.github.com>
2020-08-17 15:32:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+var getCurrentDate = window.setInterval(function() {
+  var today = new Date();
+  var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+  document.getElementById("currentTime").innerHTML = date
+}, 1000)
+
 
