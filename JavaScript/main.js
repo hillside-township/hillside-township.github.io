8f8a149bce2a2d6f9636519e4f145dc8dc4e4af5
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-11-03 16:27:28 -0500

    hi

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,3 +3,7 @@ var getCurrentDate = window.setInterval(function() {
   var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
   document.getElementById("currentTime").innerHTML = date
 }, 1000)
+
+//window.onbeforeunload = function(){
+ // return 'Are you sure you want to leave?';
+//};
