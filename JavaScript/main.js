39506bbbdff6f86848cea279dfaf4efa0ef1a20d
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-26 10:49:15 -0500

    Right Click Menu Progress!

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -7,3 +7,36 @@ var getCurrentDate = window.setInterval(function() {
 //window.onbeforeunload = function(){
  // return 'Are you sure you want to leave?';
 //};
+
+// Right Click Menu
+let rightClickMenu = document.getElementById("menu").style;
+if (document.addEventListener) {
+   document.addEventListener('contextmenu', function(e) {
+      var posX = e.clientX;
+      var posY = e.clientY;
+      menu(posX, posY);
+      e.preventDefault();
+   }, false);
+   document.addEventListener('click', function(e) {
+      rightClickMenu.display = "none";
+   }, false);
+}
+else {
+   document.attachEvent('oncontextmenu', function(e) {
+      var posX = e.clientX;
+      var posY = e.clientY;
+      menu(posX, posY);
+      e.preventDefault();
+   });
+   document.attachEvent('onclick', function(e) {
+      setTimeout(function() {
+         rightClickMenu.display = "none";
+      }, 501);
+   });
+}
+
+function menu(x, y) {
+   rightClickMenu.top = y + "px";
+   rightClickMenu.left = x + "px";
+   rightClickMenu.display = "block";
+}
