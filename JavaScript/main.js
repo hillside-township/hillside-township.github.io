6d131d1150b208518ccbcebdffee3e2ad2232633
Squirrel-314 <schoolarlybay@gmail.com>
2021-02-12 13:29:36 -0500

    Navbar edits!

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,38 +3,3 @@ var getCurrentDate = window.setInterval(function() {
   var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
   document.getElementById("currentTime").innerHTML = date
 }, 1000)
-
-//==========================================================
-// Right Click Button
-//==========================================================
-
-let rightClickMenu = document.getElementById("menu").style;
-if (document.addEventListener) {
-   document.addEventListener('contextmenu', function(e) {
-      var posX = e.clientX;
-      var posY = e.clientY;
-      menu(posX, posY);
-      e.preventDefault();
-   }, false);
-   document.addEventListener('click', function(e) {
-      rightClickMenu.display = "none";
-   }, false);
-}
-else {
-   document.attachEvent('oncontextmenu', function(e) {
-      var posX = e.clientX;
-      var posY = e.clientY;
-      menu(posX, posY);
-      e.preventDefault();
-   });
-   document.attachEvent('onclick', function(e) {
-      setTimeout(function() {
-         rightClickMenu.display = "none";
-      }, 501);
-   });
-}
-function menu(x, y) {
-   rightClickMenu.top = y + "px";
-   rightClickMenu.left = x + "px";
-   rightClickMenu.display = "block";
-}
