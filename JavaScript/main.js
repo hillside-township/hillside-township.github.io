08de8a577d3112e9bde7f2d657e0f5237ccd5146
Squirrel-314 <schoolarlybay@gmail.com>
2021-04-20 11:38:20 -0400

    Parrelex Scroll

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,3 +3,9 @@ var getCurrentDate = window.setInterval(function() {
   var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
   document.getElementById("currentTime").innerHTML = date
 }, 1000)
+
+// Choose Image
+let updateBackgroundImg = window.setInterval(function() {
+   let backgroundImg = ["Images/plant-invasion.jpg", "Images/messy-house.jpg", "Images/fallen-traffic-cone.jpg", "Images/trees.jpg", "Images/uninhabited.jpg", "Images/wall.jpg", "Images/creeping-vines.jpg"][Math.floor(Math.random() * 7)];
+   document.querySelector("#title-background").style.backgroundImage = `url("${backgroundImg}")`;
+}, 5000)
